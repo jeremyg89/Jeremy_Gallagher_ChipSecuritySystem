0670bd03bb2a2bb659123a254a20eaf57943582e
jeremy.gallagher <jeremy.gallagher@skyepack.com>
2026-09-04 14:32:02 -0400

    Add project files.

---
+++ ChipChainSolver.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections.Generic;
+
+namespace ChipSecuritySystem
+{
+    /// <summary>
+    /// Finds the longest chain from Blue to Green. Doesn't reuse chips.
+    /// </summary>
+    public class ChipChainSolver
+    {
+        /// <summary>
+        /// Looks for the longest Blue to Green chain.
+        /// </summary>
+        public IList<ColorChip> FindLongestChain(IList<ColorChip> chips)
+        {
+            // No chips, can't unlock.
+            if (chips == null || chips.Count == 0)
+            {
+                return null;
+            }
+
+            List<ColorChip> optimalChain = null;
+            var chain = new List<ColorChip>();
+            var used = new bool[chips.Count];
+
+            Search(chips, Color.Blue, used, chain, ref optimalChain);
+            return optimalChain;
+        }
+
+        /// <summary>
+        /// Gets chips that didn't get used in the chain.
+        /// </summary>
+        public IList<ColorChip> GetUnusedChips(IList<ColorChip> chips, IList<ColorChip> chain)
+        {
+            var unusedChips = new List<ColorChip>();
+
+            if (chips == null || chips.Count == 0)
+            {
+                return unusedChips;
+            }
+
+            foreach (ColorChip chip in chips)
+            {
+                bool wasUsed = false;
+                if (chain != null)
+                {
+                    foreach (ColorChip usedChip in chain)
+                    {
+                        if (object.ReferenceEquals(chip, usedChip))
+                        {
+                            wasUsed = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!wasUsed)
+                {
+                    unusedChips.Add(chip);
+                }
+            }
+
+            return unusedChips;
+        }
+
+        /// <summary>
+        /// Tries unused chips from the current color and keeps the longest chain that ends on Green.
+        /// </summary>
+        private static void Search(
+            IList<ColorChip> chips,
+            Color currentColor,
+            bool[] used,
+            List<ColorChip> chain,
+            ref List<ColorChip> optimalChain)
+        {
+            // Checks to see if it ends in green. Keep looking in case there's a longer one.
+            if (chain.Count > 0 && currentColor == Color.Green)
+            {
+                if (optimalChain == null || chain.Count > optimalChain.Count)
+                {
+                    optimalChain = new List<ColorChip>(chain);
+                }
+
+                if (optimalChain.Count == chips.Count)
+                {
+                    return;
+                }
+            }
+
+            // Try chips that aren't used yet and match the current color.
+            for (int i = 0; i < chips.Count; i++)
+            {
+                if (used[i] || chips[i].StartColor != currentColor)
+                {
+                    continue;
+                }
+
+                used[i] = true;
+                chain.Add(chips[i]);
+
+                Search(chips, chips[i].EndColor, used, chain, ref optimalChain);
+
+                chain.RemoveAt(chain.Count - 1);
+                used[i] = false;
+
+                if (optimalChain != null && optimalChain.Count == chips.Count)
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChipSecuritySystem
+{
+    class Program
+    {
+        /// <summary>
+        /// Runs a few sample bags and prints the results.
+        /// </summary>
+        static void Main()
+        {
+            // Empty bag, can't unlock.
+            var exampleBagEmpty = new List<ColorChip>();
+
+            // Readme example. Orange/Purple isn't used.
+            var exampleBagReadmeExample = new List<ColorChip>()
+            {
+                new ColorChip(Color.Blue, Color.Yellow),
+                new ColorChip(Color.Red, Color.Green),
+                new ColorChip(Color.Yellow, Color.Red),
+                new ColorChip(Color.Orange, Color.Purple),
+            };
+
+            // Longer bag with two blues.
+            var exampleBagSevenChips = new List<ColorChip>()
+            {
+                new ColorChip(Color.Blue, Color.Yellow),
+                new ColorChip(Color.Red, Color.Green),
+                new ColorChip(Color.Orange, Color.Red),
+                new ColorChip(Color.Orange, Color.Purple),
+                new ColorChip(Color.Purple, Color.Blue),
+                new ColorChip(Color.Yellow, Color.Orange),
+                new ColorChip(Color.Blue, Color.Orange)
+            };
+
+            // Longer bag with two blues and two greens.
+            var exampleBagEightChips = new List<ColorChip>()
+            {
+                new ColorChip(Color.Blue, Color.Yellow),
+                new ColorChip(Color.Red, Color.Green),
+                new ColorChip(Color.Orange, Color.Red),
+                new ColorChip(Color.Orange, Color.Purple),
+                new ColorChip(Color.Purple, Color.Blue),
+                new ColorChip(Color.Yellow, Color.Orange),
+                new ColorChip(Color.Blue, Color.Orange),
+                new ColorChip(Color.Red, Color.Green)
+            };
+
+            SolveChain(exampleBagEmpty);
+            SolveChain(exampleBagReadmeExample);
+            SolveChain(exampleBagSevenChips);
+            SolveChain(exampleBagEightChips);
+        }
+
+        /// <summary>
+        /// Prints the chain, or the error if it can't unlock.
+        /// </summary>
+        static void SolveChain(List<ColorChip> exampleBag)
+        {
+            var chipChainSolver = new ChipChainSolver();
+
+            IList<ColorChip> chain = chipChainSolver.FindLongestChain(exampleBag);
+
+            if (chain == null || chain.Count == 0)
+            {
+                Console.WriteLine(Constants.ErrorMessage + "\n");
+            }
+            else
+            {
+                Console.WriteLine(FormatChain(chain));
+
+                // On successful chain, print leftover chips if there are any.
+                IList<ColorChip> unusedChips = chipChainSolver.GetUnusedChips(exampleBag, chain);
+                Console.WriteLine(FormatUnusedChips(unusedChips));
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// Prints the chain the same way as the readme.
+        /// </summary>
+        static string FormatChain(IList<ColorChip> chain)
+        {
+            return "The result that successfully links the blue and green markers is:\r\n\r\nBlue"
+                + FormatChipList(chain)
+                + " Green\n";
+        }
+
+        /// <summary>
+        /// Prints leftover chips.
+        /// </summary>
+        static string FormatUnusedChips(IList<ColorChip> unusedChips)
+        {
+            if (unusedChips == null || unusedChips.Count == 0)
+            {
+                return "Unused chips: none";
+            }
+
+            return "Unused chips:" + FormatChipList(unusedChips);
+        }
+
+        /// <summary>
+        /// Formats each chip in brackets.
+        /// </summary>
+        static string FormatChipList(IList<ColorChip> chips)
+        {
+            var builder = new StringBuilder();
+
+            foreach (ColorChip chip in chips)
+            {
+                builder.Append(" [");
+                builder.Append(chip);
+                builder.Append("]");
+            }
+
+            return builder.ToString();
+        }
+    }
+}
